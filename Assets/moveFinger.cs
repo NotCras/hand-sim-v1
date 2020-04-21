23e4623e0f2e26b64d8458077e84b7b66cc0e445
John <morrowjo@oregonstate.edu>
2020-04-21 15:31:31 -0700

    added unity project files
    also imported basic finger model

---
+++ Assets/moveFinger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class moveFinger : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("in m/s")][SerializeField] private float speed = 10f;
+    
+    [Header("Control-based parameters")]
+    [SerializeField] private float controlFactor = 10f;
+
+    private float finger_throw;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finger_throw = CrossPlatformInputManager.GetAxis("Horizontal");
+        
+        
+    }
+}
